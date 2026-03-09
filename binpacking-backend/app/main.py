8bce6989fdeefa6bd9ecc4666fd56e8937753d6b
Abhedya1811 <abhedyabhole8@gmail.com>
2026-03-09 21:42:28 +0530

    fix dependencies2

--- binpacking-backend/app/main.py
+++ binpacking-backend/app/main.py
@@ -23,7 +23,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "https://ashwell3d-binpackaging.vercel.app"
+        "https://ashwell3d-binpackaging.vercel.app",
+         "https://ashwell3d-binpackaging-80tslnhqk-abhedya1811s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
