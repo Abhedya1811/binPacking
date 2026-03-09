bf9f852dc3d2c6e0af42dc9f9e11b2ef15e323cf
Abhedya1811 <abhedyabhole8@gmail.com>
2026-03-09 21:19:35 +0530

    frontendDone

--- binpacking-frontend/src/services/api.js
+++ binpacking-frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
-
+const  API_BASE_URL = import.meta.env.VITE_API_URL;
 export const api = {
   async calculatePacking(data) {
     console.log('📤 [api.js] Received data:', data);
